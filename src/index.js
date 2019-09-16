924e1b4ddf908040f3d6e7e7b63ca859fe87fc8f
puztli <43328895+puztli@users.noreply.github.com>
2019-09-16 17:28:46 +0900

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function Game() {
+  return (
+    <h1></h1>
+  );
+}
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
