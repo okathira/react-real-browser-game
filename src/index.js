44e9eec0475caaa34c1a9a9908992894a4b609e6
puztli <43328895+puztli@users.noreply.github.com>
2019-09-30 07:46:35 +0900

    Make KeyInputManager work correctly
    and add display of key input for debug.

--- src/index.js
+++ src/index.js
@@ -17,7 +17,16 @@ class Game extends React.Component {
     super(props);
     this.state = {
       mainWindowHandle: undefined,
+      keyInputs: Array(256).fill(false),
     }
+    this.createMainWindow = this.createMainWindow.bind(this);
+    this.refreshKeyInput = this.refreshKeyInput.bind(this);
+  }
+
+  refreshKeyInput(keyCode, isDown) {
+    const keyInputs = this.state.keyInputs.slice();
+    keyInputs[keyCode] = isDown;
+    this.setState({ keyInputs: keyInputs });
   }
 
   createMainWindow() {
@@ -30,8 +39,9 @@ class Game extends React.Component {
     return (
       <div className="game">
         <div className="controller">
-          <CreateWindowButton onClick={() => this.createMainWindow()} />
+          <CreateWindowButton onClick={this.createMainWindow} />
         </div>
+        <KeyInputManager refreshKeyInput={this.refreshKeyInput} />
       </div>
     );
   }
@@ -41,8 +51,10 @@ class KeyInputManager extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      inputKeys: new Array(128).fill(false),
-    };
+      latestInput: { key: "", state: "" },
+    }
+    this.onKeyDown = this.onKeyDown.bind(this);
+    this.onKeyUp = this.onKeyUp.bind(this);
   }
 
   componentDidMount() {
@@ -56,19 +68,21 @@ class KeyInputManager extends React.Component {
   }
 
   onKeyDown(event) {
-    let crntInputKeys = this.state.inputKeys;
-    crntInputKeys[event.keyCode] = true;
-    this.setState({
-      inputKeys: crntInputKeys,
-    })
+    this.props.refreshKeyInput(event.keyCode, true);
+    this.setState({ latestInput: { key: event.keyCode, state: "is down" } });
   }
 
   onKeyUp(event) {
-    let crntInputKeys = this.state.inputKeys;
-    crntInputKeys[event.keyCode] = false;
-    this.setState({
-      inputKeys: crntInputKeys,
-    })
+    this.props.refreshKeyInput(event.keyCode, false);
+    this.setState({ latestInput: { key: event.keyCode, state: "is up" } });
+  }
+
+  render() {
+    return (
+      <h3>
+        Latest Input: {this.state.latestInput.key} {this.state.latestInput.state}
+      </h3>
+    );
   }
 }
 
@@ -76,8 +90,6 @@ function createWindow(url, windowName, windowFeatures) {
   const windowHandle = window.open(url, windowName, windowFeatures);
   return windowHandle;
 }
-
-
 
 ReactDOM.render(
   <Game />,
