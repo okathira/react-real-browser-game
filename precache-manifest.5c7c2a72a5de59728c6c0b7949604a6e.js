9538a4989593c563c47afcdf84e185c4acb3b60a
puztli <43328895+puztli@users.noreply.github.com>
2019-10-11 02:36:07 +0900

    Updates

---
+++ precache-manifest.5c7c2a72a5de59728c6c0b7949604a6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "622de8b5e6a7006dab9b17a0959b2bb3",
+    "url": "/react-real-browser-game/index.html"
+  },
+  {
+    "revision": "6755f6812a5bc604d410",
+    "url": "/react-real-browser-game/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "de4dc511876ba78f9343",
+    "url": "/react-real-browser-game/static/js/2.2bf16d2b.chunk.js"
+  },
+  {
+    "revision": "6755f6812a5bc604d410",
+    "url": "/react-real-browser-game/static/js/main.9dd151d9.chunk.js"
+  },
+  {
+    "revision": "1af83164c4c17334b25e",
+    "url": "/react-real-browser-game/static/js/runtime~main.69c62417.js"
+  }
+]);
