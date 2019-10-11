55e0bee048fe2dc38f55dad40057483e929d8cca
puztli <43328895+puztli@users.noreply.github.com>
2019-10-11 14:14:00 +0900

    Stage /build for Unubo

---
+++ build/precache-manifest.4a28da4035dfb98ff235d3eb4b7d5576.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ae7cf26d78825053132aec5356de081",
+    "url": "/react-real-browser-game/index.html"
+  },
+  {
+    "revision": "cf276e62c358414cab7b",
+    "url": "/react-real-browser-game/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "de4dc511876ba78f9343",
+    "url": "/react-real-browser-game/static/js/2.2bf16d2b.chunk.js"
+  },
+  {
+    "revision": "cf276e62c358414cab7b",
+    "url": "/react-real-browser-game/static/js/main.d132f676.chunk.js"
+  },
+  {
+    "revision": "1af83164c4c17334b25e",
+    "url": "/react-real-browser-game/static/js/runtime~main.69c62417.js"
+  }
+]);
